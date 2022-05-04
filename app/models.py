16e6ee81cfb60a1c46238241786c2d18e78ac06d
Yupeng Li <lypa520@gmail.com>
2022-05-04 16:46:26 +0800

    Added flask-login module & bug fixed: for creating duplicate database

--- app/models.py
+++ app/models.py
@@ -1,24 +1,29 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from config import db,login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-#get absolute path
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
-
-# Configure database path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'flask_sql.db')
-# Disable dynamic tracking of database modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# instantiate database
-db = SQLAlchemy(app)
+#Get the user object and store it in the session
+@login_manager.user_loader
+def load_user(user_id):
+    return UserModel.query.get(int(user_id))
 
 # user model
-class UserModel(db.Model):
+class UserModel(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(200), nullable=False)
+    hash_password = db.Column(db.String(200), nullable=False)
+
+    # convert password to hash value
+    def encode_password(password):
+        #hash value of password
+        hash_password = generate_password_hash(password)
+        return hash_password
+
+    # Verify if the hash value of password is equal to the input password
+    def decode_password(hash_password, password):
+        check_password = check_password_hash(hash_password, password)
+        return check_password #True or False
 
 # question model
 class QuestionModel(db.Model):
@@ -27,41 +32,18 @@ class QuestionModel(db.Model):
     content = db.Column(db.Text,nullable=False)
     answer = db.Column(db.String(4), nullable=False)
 
-# read QA.txt file and store questions and answers in QuestionModel
-def read_file(filename):
-    with open(filename, 'r') as f:
-        content = f.readlines()
-    for line in content:
-        line = line.strip()
-        line = line.split('\t')
-        question = QuestionModel(content=line[1], answer=line[0])
-        db.session.add(question)
-    db.session.commit()
+class FileReader():
+    # read QA.txt file and store questions in database
+    def read_file(db,filename):
+        with open(filename, 'r') as f:
+            content = f.readlines()
+        for line in content:
+            line = line.strip()
+            line = line.split('\t')
+            question = QuestionModel(id=[0], answer=line[1], content=line[2])
+            db.session.add(question)
+        try:
+            db.session.commit()
+        except:
+            print("---Duplicate Database Created---")
 
-
-'''
-instantiate database：
-   1. Python env: python3
-   2. from [yourapplication] import db
-    >>> db.create_all()
-   3. from [yourapplication] import [Role] //UserModel instead of users
-    >>> admin = UserModel(username='admin')
-    >>> guest = UserModel(username='guest') 
-   4. 
-   >>> db.session.add(admin)
-   >>> db.session.add(guest)
-   >>> db.session.commit() //.seesion.commit()
-
-'''
-
-
-if __name__ == '__main__':
-    # db.drop_all()
-    db.create_all()
-    admin = UserModel(username='admin',password='admin')
-    q1 = QuestionModel(content='question 1 ', answer='1949')
-    db.session.add(q1)
-    db.session.add(admin)
-    db.session.commit()
-    # app.run(debug=True)
-    read_file('static/questions.txt')
