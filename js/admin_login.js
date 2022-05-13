2fd4a6abfeb6282eeb491f71ed84963690c191e3
MelvinHuangDongDong <103872839+MelvinHuangDongDong@users.noreply.github.com>
2022-05-13 19:14:50 +0800

    Separate JS into separate file admin_login.js

---
+++ js/admin_login.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	$("#submit").click(function(){
+		event.preventDefault();
+		var username = $("#username").val().trim();
+		var password = $("#password").val().trim();
+		var remember = false;
+		if(username.length == 0 ){
+			alert("Please input username");
+			return;
+		}
+		if(password.length == 0 ){
+			alert("Please input password");
+			return;
+		}
+		if($("#remember_me").is(':checked')){
+			remember = true;
+		}
+		$.ajax({
+			url: '/admin_login',
+			type: 'POST',
+			dataType: 'json',
+			data: {'username': username, 'password': password, 'remember': remember},
+			success: function(data, textStatus, xhr) {
+				if(data.status == 'success'){
+					window.location.href = '/admin_index.html';
+				}else{
+					alert("The user name does not match the password");
+				}
+			},
+			error: function(xhr, textStatus, errorThrown) {
+			//called when there is an error
+			}
+				
+		});
+	});
+});
