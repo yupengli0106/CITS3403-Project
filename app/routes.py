45ca90fdb1992d385fa6a5ca22d03ff500de0574
Yupeng Li <lypa520@gmail.com>
2022-05-18 14:44:52 +0800

    add the admin to search question by keywords function & bug fixed that user get duplicate questions

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,11 @@ from flask_login import login_user, logout_user, login_required, current_user
 from flask import render_template, request, flash
 from sqlalchemy import func
 from models import UserModel, QuestionModel, FileReader, ScoreModel
-mylist = []  # store question id
 
 # ---------------------HTML pages-----------------------------------------------------------------------------------------------------------------------------------------------------------
 @app.route('/')
 @app.route('/login.html', methods=['GET', 'POST'])
-def index():
+def login_page():
     return render_template('login.html')
 
 @app.route('/register.html', methods=['POST', 'GET'])
@@ -17,19 +16,26 @@ def register_page():
 
 @app.route('/game.html', methods=['POST', 'GET'])
 @login_required
-def game():
+def game_page():
     return render_template('game.html')
 
 @app.route('/profile.html', methods=['POST', 'GET'])
 @login_required
-def test():
+def profile_page():
     return render_template('profile.html')
+
+@app.route('/admin_login.html', methods=['POST', 'GET'])
+def admin_login_page():
+    return render_template('admin_login.html')
+
+@app.route('/admin_index.html', methods=['POST', 'GET'])
+@login_required
+def admin_index_page():
+    return render_template('admin_index.html')
 
 # ---------------------basic functions-----------------------------------------------------------------------------------------------------------------------------------------------------------
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    # clear mylist[] when a new user login
-    mylist.clear()
     if request.method == 'POST':
         name = request.form.get('username')
         pwd = request.form.get('password')
@@ -46,6 +52,22 @@ def login():
         print('login failed, username or password is incorrect')
         return {'status': 'fail'}
     return render_template('login.html')
+
+@app.route('/admin_login', methods=['POST', 'GET'])
+def admin_login():
+    if request.method == 'POST':
+        name = request.form.get('username')
+        pwd = request.form.get('password')
+        user = UserModel.query.filter_by(username=name).first()
+        if user is not None and user.decode_password(pwd):
+            curr_user = UserModel()
+            curr_user.id = user.id
+            curr_user.username = user.username
+            login_user(curr_user, remember=request.form.get('remember'))
+            return {'status': 'success'}
+        print('login failed, username or password is incorrect')
+        return {'status': 'fail'}
+    return render_template('admin_login.html')
 
 @app.route('/logout', methods=['POST', 'GET'])
 @login_required
@@ -79,7 +101,8 @@ def register():
     return render_template('register.html')
 
 @app.route('/questions', methods=['POST', 'GET'])
-# ramdomly generate a question from QuestionModel and can't be repeated
+@login_required
+# ramdomly generate a question from QuestionModel
 def get_question():
     while True:
         question = QuestionModel.query.order_by(func.random()).first()
@@ -87,12 +110,14 @@ def get_question():
             question_id = question.id
             ques = question.content
             ans = question.answer
-            if question_id in mylist:
+             # if the user has answered all the questions then return game over
+            if ScoreModel.query.filter_by(user_id=current_user.id).count() == QuestionModel.query.count():
+                return "game over"
+             # check if the question has been answered correctly by the user
+            if ScoreModel.query.filter_by(user_id=current_user.id, ques_id=question.id).first() is not None:
                 continue
-            mylist.append(question_id)
-            print(mylist)
             return {'question_id':question_id,'question': ques, 'answer': ans}
-        return "no more question"
+        return {'status': 'fail'}
 
 # ---------------------user profile-----------------------------------------------------------------------------------------------------------------------------------------------------------
 @app.route('/update', methods=['POST', 'GET'])
@@ -125,6 +150,12 @@ def update_user():
 # delete current user from database
 def delete_user():
     user = UserModel.query.filter_by(id=current_user.id).first()
+    user_records = ScoreModel.query.filter_by(user_id=current_user.id).all()
+    if user_records is not None:
+        # delete all records of current user
+        for record in user_records:
+            db.session.delete(record)
+    # delete current user
     db.session.delete(user)
     try:
         db.session.commit()
@@ -178,6 +209,20 @@ def get_questions():
             result.append({'id': question.id, 'content': question.content, 'answer': question.answer})
         return {'question_list': result}
     return {'status': 'fail'}
+
+@app.route('/admin_search_keyword', methods=['POST', 'GET'])
+# search question by keywords in question content
+def search_by_keywords():
+    if request.method == 'POST':
+        keyword = request.form.get('keyword')
+        questions = QuestionModel.query.filter(QuestionModel.content.like('%'+keyword+'%')).all()
+        result=[]#result list will be empty if cannot find the keyword in all question content
+        if questions is not None:
+            for question in questions:
+                result.append({'id': question.id, 'content': question.content, 'answer': question.answer})
+            return {'question_list': result}
+        return {'status': 'fail'}
+    return render_template('admin_index.html')
 
 @app.route('/admin_get_question/<int:question_id>', methods=['POST', 'GET'])
 # get a question by question id
